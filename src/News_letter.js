bd85ccebe9dad439b75b640307412a4283c7ed79
YogiboKorea <yogico.design@gmail.com>
2024-09-03 09:45:21 +0900

    Up

--- src/News_letter.js
+++ src/News_letter.js
@@ -1,56 +1,49 @@
-import './css/section_01.css'
-import './css/news.css'
-import './css/careers.css'
+import './css/section_01.css';
+import './css/news.css';
+import './css/careers.css';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { changeName } from './store.js'
 
-function News_letter(){
-   let state = useSelector((state)=> state)
-    return(
-       <>
-       <section class="p_l_d_b" style={{paddingTop:'20px!important'}}>
-       <div class="boarder_wrapper" data-aos="fade-up" data-aos-duration="800">
-          <div class="row">
-            <News_border/>
-          </div>
-       </div>
-       </section>
-         
-       </>
-    )
-         function News_border(){
-            return(
-               <>
+function News_letter() {
+    const newsBord = useSelector((state) => state.news_bord);
 
-               {
-                 state.news_bord.map(function(a,i){
-                        return(
-                           <div class="board_list col-xl-4 col-md-4 mgt_20">
-                           <dl>
-                              <dt class="board_img">{state.news_bord[i].img}</dt>
-                              <dd>
-                                 <div class="board_title">
+    return (
+        <>
+            <section className="p_l_d_b" style={{ paddingTop: '20px' }}>
+                <div className="boarder_wrapper" data-aos="fade-up" data-aos-duration="800">
+                    <div className="row">
+                        <News_border newsBord={newsBord} />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+
+    function News_border({ newsBord }) {
+        return (
+            <>
+                {newsBord.map((a, i) => (
+                    <div key={i} className="board_list col-xl-4 col-md-4 mgt_20">
+                        <dl>
+                            <dt className="board_img">{newsBord[i].img}</dt>
+                            <dd>
+                                <div className="board_title">
                                     <dl>
-                                       <dt>{state.news_bord[i].title}</dt>
-                                       <dd>{state.news_bord[i].sub_title}</dd>
-                                    </dl>		
-                                 </div>
-                                 <div class="board_cont">
-                                    {state.news_bord[i].sub_content}
-                                 </div>
-                                 <div class="board_more_button">{state.news_bord[i].more}</div>
-                              </dd>
-                           </dl>
-                        </div>
-                        )
+                                        <dt>{newsBord[i].title}</dt>
+                                        <dd>{newsBord[i].sub_title}</dd>
+                                    </dl>
+                                </div>
+                                <div className="board_cont">
+                                    {newsBord[i].sub_content}
+                                </div>
+                                <div className="board_more_button">{newsBord[i].more}</div>
+                            </dd>
+                        </dl>
+                    </div>
+                ))}
+            </>
+        );
+    }
+}
 
-                  })
-               }
-                  
-               </>
-            )
-            
-         }
-   }
-   export default News_letter
+export default News_letter;
